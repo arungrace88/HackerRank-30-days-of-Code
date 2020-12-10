fb7caf38767fbc8df09471e0221730280216be92
arungrace88 <arungrace88@gmail.com>
2020-12-10 22:35:03 +0000

    Day 05 Let's Review. Initial commit.

---
+++ Day06_Lets_Review.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+def even_odd(testcase):
+    eve = [testcase[s] for s in range(0, len(testcase), 2)]
+    odd = [testcase[s] for s in range(1, len(testcase), 2)]
+    print("".join(eve), "".join(odd))
+
+T = int(input())
+
+test = [input() for i in range(T)]
+
+for i in range(T):
+    even_odd(test[i])
